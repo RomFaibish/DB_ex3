1b66f0b964da11ffe496a54de8e9127d0c30bdcf
ramGoldstein <ram.goldstein.43@gmail.com>
2024-06-17 20:12:56 +0300

    all

--- q6.sql
+++ q6.sql
@@ -17,5 +17,5 @@ WHERE NOT EXISTS((SELECT a2.conference, a2.year
                   EXCEPT
                   (SELECT a3.conference, a3.year
                   FROM authors a3
-                  WHERE a1.name = a3.name and a1.year = a3.year))
+                  WHERE a1.name = a3.name))
 ORDER BY a1.name ASC;
