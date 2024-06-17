b1017ed8dc577e3560dea0b228a8554b9e44a660
ramGoldstein <ram.goldstein.43@gmail.com>
2024-06-17 13:39:33 +0300

    q1d

--- q4.sql
+++ q4.sql
@@ -1,4 +1,4 @@
-select institution, name
+select DISTINCT institution, name
 from institutions natural join authors natural join conferences
 where country ='il' and totalcount >= 2 and conference LIKE 'sig%'
 order by institution ASC, name ASC;
