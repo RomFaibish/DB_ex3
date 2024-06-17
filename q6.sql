5769e0c1696a6016239797860318d6bb4a26e96a
ramGoldstein <ram.goldstein.43@gmail.com>
2024-06-17 20:10:07 +0300

    all

--- q6.sql
+++ q6.sql
@@ -1,11 +1,20 @@
+--SELECT DISTINCT name
+--FROM authors a,
+--    (SELECT conference, year
+--    FROM authors
+--    WHERE name = 'Omri Abend') o
+--WHERE a.conference = o.conference and o.year = a.year
+--GROUP BY name
+--HAVING count(*) >= ALL(SELECT count(*)
+--                       FROM authors
+--                       WHERE name = 'Omri Abend')
+--ORDER BY name ASC;
 SELECT DISTINCT name
-FROM authors a,
-    (SELECT conference, year
-    FROM authors
-    WHERE name = 'Omri Abend') o
-WHERE a.conference = o.conference and o.year = a.year
-GROUP BY name
-HAVING count(*) >= ALL(SELECT count(*)
-                       FROM authors
-                       WHERE name = 'Omri Abend')
+FROM authors a1
+WHERE NOT EXISTS((SELECT a2.conference, a2.year
+                  FROM author a2)
+                  EXCEPT
+                  (SELECT a3.conference, a3.year
+                  FROM author a3
+                  WHERE a1.name = a3.name and a1.year = a3.year))
 ORDER BY name ASC;
