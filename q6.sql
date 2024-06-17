91eb90637025552cbc8959aa087d1afe49db6275
ramGoldstein <ram.goldstein.43@gmail.com>
2024-06-17 20:23:11 +0300

    all

--- q6.sql
+++ q6.sql
@@ -13,9 +13,9 @@ SELECT DISTINCT a1.name
 FROM authors a1
 WHERE NOT EXISTS((SELECT a2.conference, a2.year
                   FROM authors a2
-                  WHERE a2.name = 'Omri Abend')
+                  WHERE a2.name = 'Omri Abend'
                   EXCEPT
-                  (SELECT a3.conference, a3.year
+                  SELECT a3.conference, a3.year
                   FROM authors a3
                   WHERE a1.name = a3.name and a2.year != a3.year))
 ORDER BY a1.name ASC;
