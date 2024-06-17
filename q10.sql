96bc0bc1c4c8aa124d09da15864bdc9ce403763b
ramGoldstein <ram.goldstein.43@gmail.com>
2024-06-17 17:20:40 +0300

    Merge branch 'main' of https://github.com/RomFaibish/DB_ex3
    # Conflicts:
    #	q7.sql

--- q10.sql
+++ q10.sql
@@ -7,4 +7,4 @@ SELECT a1.year, a1.name
 FROM author_year_total a1
 GROUP BY year
 HAVING a1.yearCount >=ALL(SELECT yearCount FROM author_year_total a2 WHERE a1.year=a2.year)
-ORDER BY a1.country, a1.institution;
+ORDER BY a1.year, a1.name;
