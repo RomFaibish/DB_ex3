9b43cdcde4b160d10236f40c3c41d1dc5d040b70
ramGoldstein <ram.goldstein.43@gmail.com>
2024-06-17 17:32:49 +0300

    f

--- q10.sql
+++ q10.sql
@@ -6,6 +6,7 @@ WITH author_area_total AS(
 SELECT a1.year, a1.name
 FROM author_area_total a1
 GROUP BY year,name
+where a1.year >= 2000
 HAVING SUM(a1.areaCount)>=ALL(SELECT a2.areaCount FROM author_area_total a2 WHERE a1.year=a2.year)
 ORDER BY a1.year, a1.name;
 --
