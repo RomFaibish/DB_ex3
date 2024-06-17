6a8c950dd9c1d9af9eed7a57a5de356d254519a5
RomFaibish <133245124+RomFaibish@users.noreply.github.com>
2024-06-17 14:17:49 +0300

    correct q5

--- q6.sql
+++ q6.sql
@@ -5,6 +5,6 @@ FROM authors NATURAL JOIN conferences
 EXCEPT
 SELECT name
 FROM authors NATURAL JOIN conferences
-WHERE year <= 1980 OR area = 'theory'
+WHERE year >= 1980 OR area != 'theory'
 ) T
 ORDER BY name
