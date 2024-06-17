7a3bb4fe1c0fb7a08b6f91eed9e9ab8e493501d4
ramGoldstein <ram.goldstein.43@gmail.com>
2024-06-17 17:53:49 +0300

    f

--- q10.sql
+++ q10.sql
@@ -7,7 +7,7 @@ SELECT a1.year, a1.name
 FROM author_area_total a1
 where a1.year >= 2000
 GROUP BY year, name
-HAVING MAX(a1.areaCount)>=ALL(SELECT a2.areaCount FROM author_area_total a2 WHERE a1.year=a2.year GROUP BY year, name)
+HAVING MAX(a1.areaCount)>=ALL(SELECT  MAX(a2.areaCount) FROM author_area_total a2 WHERE a1.year=a2.year GROUP BY year, name)
 ORDER BY a1.year, a1.name;
 
 --WITH author_area_total AS(
