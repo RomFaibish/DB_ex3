cf768879df3a5b5021854ad54839bb85c06acdac
ramGoldstein <ram.goldstein.43@gmail.com>
2024-06-17 17:44:46 +0300

    f

--- q10.sql
+++ q10.sql
@@ -1,22 +1,22 @@
-WITH author_area_total AS(
-    SELECT name, year, SUM(totalcount) AS areaCount
-    FROM authors
-    GROUP BY name, year
-    )
-SELECT a1.year, a1.name
-FROM author_area_total a1
-where a1.year >= 2000
-GROUP BY year, name
-HAVING a1.areaCount>=ALL(SELECT a2.areaCount FROM author_area_total a2 WHERE a1.year=a2.year)
-ORDER BY a1.year, a1.name;
---
 --WITH author_area_total AS(
---    SELECT name, year, area, SUM(totalcount) AS areaCount
---    FROM authors NATURAL JOIN conferences
---    GROUP BY name, year, area
+--    SELECT name, year, SUM(totalcount) AS areaCount
+--    FROM authors
+--    GROUP BY name, year
 --    )
 --SELECT a1.year, a1.name
 --FROM author_area_total a1
---GROUP BY year,name
---HAVING SUM(a1.areaCount)>=ALL(SELECT SUM(a2.areaCount) FROM author_area_total a2 WHERE a1.year=a2.year)
+--where a1.year >= 2000
+--GROUP BY year, name
+--HAVING a1.areaCount>=ALL(SELECT a2.areaCount FROM author_area_total a2 WHERE a1.year=a2.year)
 --ORDER BY a1.year, a1.name;
+--
+WITH author_area_total AS(
+    SELECT name, year, area, SUM(totalcount) AS areaCount
+    FROM authors NATURAL JOIN conferences
+    GROUP BY name, year, area
+    )
+SELECT a1.year, a1.name
+FROM author_area_total a1
+GROUP BY year, name
+HAVING SUM(a1.areaCount)>=ALL(SELECT SUM(a2.areaCount) FROM author_area_total a2 WHERE a1.year=a2.year GROUP BY year,name)
+ORDER BY a1.year, a1.name;
