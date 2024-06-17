2d7af56c462d77b5e50b9bf27a05d89ac1bdc2fb
ramGoldstein <ram.goldstein.43@gmail.com>
2024-06-17 15:17:19 +0300

    Merge branch 'main' of https://github.com/RomFaibish/DB_ex3
    # Conflicts:
    #	q4.sql

--- q7.sql
+++ q7.sql
@@ -1,8 +1,9 @@
 SELECT DISTINCT name
-FROM authors a
-WHERE (a.conference,a.year) IN (SELECT conference, year
-    FROM authors
-    WHERE name = 'Omri Abend') o
+FROM authors a,
+    (SELECT conference, year
+    FROM authors a2
+    WHERE a2.name = 'Omri Abend') o
+WHERE (a.conference,a.year) IN (o)
 GROUP BY a.name
 HAVING count(*) = count(o.conference)
 ORDER BY name;
