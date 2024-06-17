24e555d8545bbc2ed1296964bb5d76f691995d30
ramGoldstein <ram.goldstein.43@gmail.com>
2024-06-17 14:56:37 +0300

    Merge branch 'main' of https://github.com/RomFaibish/DB_ex3
    # Conflicts:
    #	q4.sql

---
+++ q7.sql
@@ -0,0 +1,8 @@
+SELECT name
+FROM authors a,
+    (SELECT conference, year
+    FROM authors
+    WHERE name = "Omri Abend") o
+WHERE a.conference,a.year IN o
+GROUP BY a.name
+HAVING count(*) = count(o);
