fa8d42789304c5d13e2ee6b9215fedf6ee24b27c
ramGoldstein <ram.goldstein.43@gmail.com>
2024-06-17 14:19:55 +0300

    Merge branch 'main' of https://github.com/RomFaibish/DB_ex3
    # Conflicts:
    #	q4.sql

--- q5.sql
+++ q5.sql
@@ -1,6 +1,6 @@
 SELECT a.name
 FROM authors a
-WHERE a.name NOT IN (SELECT ac1.name
-                        FROM authors natural join conferences ac1
-                       where ac1.year>1980 or ac1.area != 'theory')
-ORDER BY name;
+WHERE a.name NOT IN (SELECT ac.name
+                        FROM authors natural join conferences ac
+                       where ac.year>1980 or ac.area != 'theory')
+ORDER BY a.name;
