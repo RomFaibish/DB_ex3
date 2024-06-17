87f34558f53ac0f4b0c71af40fd38bcd96b7d3d4
RomFaibish <133245124+RomFaibish@users.noreply.github.com>
2024-06-17 13:58:44 +0300

    q4

--- q4.sql
+++ q4.sql
@@ -1,8 +1,11 @@
 SELECT DISTINCT name,year
-FROM authors NATURAL JOIN conferences
-WHERE institution LIKE 'Hebrew%' and Subarea = 'ai'
-INTERSECT
-SELECT DISTINCT name,year
-FROM authors NATURAL JOIN conferences
-WHERE institution LIKE 'Hebrew%' and Subarea = 'economics'
+FROM(
+    SELECT DISTINCT name,year
+    FROM authors NATURAL JOIN conferences
+    WHERE institution = 'Hebrew University of Jerusalem' and Subarea = 'ai'
+    INTERSECT
+    SELECT DISTINCT name,year
+    FROM authors NATURAL JOIN conferences
+    WHERE institution = 'Hebrew University of Jerusalem' and Subarea = 'economics'
+) AS intersected_name_year
 ORDER BY name,year
