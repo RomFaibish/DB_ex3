25372226832c33067e2a94518b714cab36ecb74e
ramGoldstein <ram.goldstein.43@gmail.com>
2024-06-17 18:36:12 +0300

    f

--- q9.sql
+++ q9.sql
@@ -1,10 +1,10 @@
 WITH country_publishes AS(
-    SELECT institution, country, SUM(totalcount) AS institutionCount
+    SELECT institution, country, SUM(totalcount) AS countryCount
     FROM institutions NATURAL JOIN authors
     GROUP BY institution
     )
 SELECT country, institution, countryCount
 FROM country_publishes c1
-WHERE institutionCount >= ALL(SELECT institutionCount FROM country_publishes c2 WHERE c1.country = c2.country)
+WHERE c1.countryCount >= ALL(SELECT c2.countryCount FROM country_publishes c2 WHERE c1.country = c2.country)
 ORDER BY country, institution;
 
