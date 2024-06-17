a85e3aa860859d2936b621420bcfe70c7d7f32df
RomFaibish <133245124+RomFaibish@users.noreply.github.com>
2024-06-17 16:39:35 +0300

    Q9

--- q9.sql
+++ q9.sql
@@ -3,7 +3,7 @@ WITH country_publishes AS(
     FROM institutions NATURAL JOIN authors
     GROUP BY institution
     )
-SELECT country, institution, countryCount
+SELECT c1.country, c1.institution, c1.institutionCount AS countryCount
 FROM country_publishes c1
-WHERE institutionCount >= ALL(SELECT institutionCount FROM country_publishes c2 WHERE c1.country = c2.country)
+WHERE c1.institutionCount >= ALL(SELECT institutionCount FROM country_publishes c2 WHERE c1.country = c2.country)
 ORDER BY country, institution;
