a5aa388eaffe4f541f2d8c63b360f44e964838a7
ramGoldstein <ram.goldstein.43@gmail.com>
2024-06-17 16:17:56 +0300

    Merge branch 'main' of https://github.com/RomFaibish/DB_ex3
    # Conflicts:
    #	q7.sql

--- q8.sql
+++ q8.sql
@@ -2,8 +2,8 @@ SELECT DISTINCT name
 FROM authors NATURAL JOIN conferences
 WHERE subarea = 'ml' AND totalcount > 0
 GROUP BY name
-HAVING count(*) >= 3
+HAVING count(DISTINCT conference) >= 3
 INTERSECT
 SELECT DISTINCT name
 FROM authors NATURAL JOIN conferences
-WHERE year >= 2020
+WHERE subarea = 'ml' and year >= 2020
